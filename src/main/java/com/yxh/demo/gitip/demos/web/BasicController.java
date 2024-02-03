6de86ce6b43ea7fdcc21afc7f451a4871f5b3f04
jackyhua <jackyhua@vip.qq.com>
2024-02-03 13:59:01 +0800

    编写完成

--- src/main/java/com/yxh/demo/gitip/demos/web/BasicController.java
+++ src/main/java/com/yxh/demo/gitip/demos/web/BasicController.java
@@ -17,7 +17,6 @@
 package com.yxh.demo.gitip.demos.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import java.net.InetAddress;
