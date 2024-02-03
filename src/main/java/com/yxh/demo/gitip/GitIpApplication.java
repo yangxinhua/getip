a9d47543300296dc380e0e532de8057245727fb1
jackyhua <jackyhua@vip.qq.com>
2024-02-03 12:07:37 +0800

    编写完成

---
+++ src/main/java/com/yxh/demo/gitip/GitIpApplication.java
@@ -0,0 +1,13 @@
+package com.yxh.demo.gitip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitIpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitIpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yxh/demo/gitip/GitIpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yxh.demo.gitip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
